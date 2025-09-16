9ba7e7e1cbba10cdaf0a4163953c090132ef62d2
Isaac <adeutisunday67@gmail.com>
2025-09-16 15:47:30 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
 console.log("Hello, Humber!");
-
-let userInput = prompt("Welcome! Please enter a number: ");
-let userInput2 = prompt("Enter a new number: ");
-let calc = userInput * userInput2;
- alert("The result of multiplying your two numbers is: " + calc);
